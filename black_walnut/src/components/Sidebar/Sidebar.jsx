3aff887381aeb07e6af6f43afb7b6812456423c3
Dima <dzybystim@gmail.com>
2025-10-28 19:01:13 +0200

    1

---
+++ black_walnut/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,10 @@
+export function Sidebar() {
+    return (
+        <ul>
+            <li>Home</li>
+            <li>Headphones</li>
+            <li>Speakers</li>
+            <li>Earphones</li>
+        </ul>
+    )
+}
